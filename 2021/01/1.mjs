5d057e15e22bda9aac6f9ab243bd8a131ca158cb
Matt Swensen <matt@mjswensen.com>
2021-12-01 05:38:19 +0000

    Day 1

--- 2021/01/1.mjs
+++ 2021/01/1.mjs
@@ -1,3 +1,11 @@
 import { readInputLines } from '../../utils.mjs';
 
-console.log(await readInputLines(import.meta.url));
+const lines = await readInputLines(import.meta.url);
+
+const answer = lines
+  .map((line) => parseInt(line, 10))
+  .reduce(
+    (inc, line, i, arr) => (i !== 0 && arr[i - 1] < line ? inc + 1 : inc),
+    0,
+  );
+console.log(answer);
---
+++ 2021/01/2.mjs
@@ -0,0 +1,18 @@
+import { readInputLines } from '../../utils.mjs';
+
+const lines = await readInputLines(import.meta.url);
+
+const answer = lines
+  .map((line) => parseInt(line, 10))
+  .reduce((acc, line, i, arr) => {
+    if (i <= 2) {
+      return acc;
+    } else {
+      return [...acc, arr[i] + arr[i - 1] + arr[i - 2]];
+    }
+  }, [])
+  .reduce(
+    (inc, sum, i, arr) => (i !== 0 && arr[i - 1] < sum ? inc + 1 : inc),
+    0,
+  );
+console.log(answer);
